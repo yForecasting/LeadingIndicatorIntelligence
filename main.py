30b01c6039186aaf4a3fe49296d641c1548a7fda
Dwight Foster <dwightfoster03@gmail.com>
2021-05-19 11:54:51 -0400

    Saving monthly data to csv

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sklearn
 import numpy as np
 
 fred = Fred(api_key='3c6ba58d26525f17af95af4fabed24be')
-monthly_list = fred.search('monthly', limit=1000, order_by='popularity', sort_order='desc',
+monthly_list = fred.search('monthly', limit=100, order_by='popularity', sort_order='desc',
                            filter=('frequency', 'Monthly'))
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 data = []
@@ -18,3 +18,4 @@ data = pd.concat(data, axis=1)
 data.columns = columns
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print(data.head())
+data.to_csv("monthly_data.csv")
